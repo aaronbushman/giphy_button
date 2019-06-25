1dd136718d340320797335eb11368328bb5452ea
aaron <aaronbushman@gmail.com>
2019-06-24 23:42:04 -0400

    added function to js to create new button from text box upon button click

--- giphy.js
+++ giphy.js
@@ -1,11 +1,21 @@
 // create array of buttons to initialy create with loop to page screen
 var topics = ["funkadelic", "james brown", "mc5", "iggy pop", "john lennon", "neil young", "sky saxon", "brian wilson", "miles davis", "john coltrane", "sun ra", "velvet underground"];
+console.log(topics);
 
 // function to create loop to push array to buttons
-function buttonPush (array) {
+function buttonPush(array) {
     for (var i = 0; i < topics.length; i++) {
         document.getElementById("buttons").innerHTML += "<button>" + array[i] + "</button>";
     }
-  }
+}
+
+$("#searchButton").click(function() {
+    var value = $("#searchBox").val();
+    $("#buttons").append('<button id="' + value + '">' + value + '</button>');
+  })
+
 
 buttonPush(topics);
+
+
+
