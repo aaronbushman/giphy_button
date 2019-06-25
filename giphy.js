e08745473ab5aeb73edb7ca61bc438905ae3a749
aaron <aaronbushman@gmail.com>
2019-06-24 22:18:12 -0400

    initial js, create loop to push buttons from array strings.

--- giphy.js
+++ giphy.js
@@ -1,5 +1,11 @@
 // create array of buttons to initialy create with loop to page screen
 var topics = ["funkadelic", "james brown", "mc5", "iggy pop", "john lennon", "neil young", "sky saxon", "brian wilson", "miles davis", "john coltrane", "sun ra", "velvet underground"];
 
+// function to create loop to push array to buttons
+function buttonPush (array) {
+    for (var i = 0; i < topics.length; i++) {
+        document.getElementById("buttons").innerHTML += "<button>" + array[i] + "</button>";
+    }
+  }
 
-
+buttonPush(topics);
