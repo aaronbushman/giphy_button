e9166b8ce294f2b925d97ef1770891d45305dfd4
aaron <aaronbushman@gmail.com>
2019-06-25 15:38:14 -0400

    updated code to pull gifs from api data and post to div, issues when hitting enter from text box causing script to reset

--- giphy.js
+++ giphy.js
@@ -2,14 +2,16 @@
 var topics = ["funkadelic", "james brown", "mc5", "iggy pop", "john lennon", "neil young", "sky saxon", "brian wilson", "miles davis", "john coltrane", "sun ra", "velvet underground"];
 console.log(topics);
 
+var random = Math.floor(Math.random() * 9) + 1;
 
 function gifPush() {
-    $("button").click(function () {
+    $("button").on("click", function (event) {
+        event.preventDefault();
         var search = $(this).attr("id");
         console.log(search);
-        // Constructing a URL to search Giphy for the name of the person who said the quote
+
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            search + "&api_key=AvjZRtvRW63wJ6k9uypihgqDBWeA2xPG&limit=1";
+            search + "&api_key=AvjZRtvRW63wJ6k9uypihgqDBWeA2xPG&limit=10";
 
         console.log(queryURL);
 
@@ -19,26 +21,20 @@ function gifPush() {
         })
 
             .then(function (response) {
-                // Storing an array of results in the results variable
                 var results = response.data;
-
-                console.log(results);
 
                 var gifCard = $("<div class='card'>");
 
-                // Creating a paragraph tag with the result item's rating
-                var gifRating = $("<div class='card-body'>").text("Rating: " + results.rating);
-    
-                // Creating and storing an image tag
+                var gifRating = $("<div class='card-body'>").text("Rating: " + results[Math.floor(Math.random() * 9) + 1].rating);
+
                 var searchGif = $("<img>");
-                // Setting the src attribute of the image to a property pulled off the result item
-                searchGif.attr("src", results.images.fixed_height.url);
-    
-                // Appending the paragraph and image tag to the animalDiv
+
+                searchGif.attr("src", results[Math.floor(Math.random() * 9) + 1].images.fixed_width.url);
+
                 gifCard.append(gifRating);
+
                 gifCard.append(searchGif);
-    
-                // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+
                 $("#gifs").prepend(gifCard);
             })
 
@@ -47,19 +43,26 @@ function gifPush() {
 
 // function to create loop to push array to buttons
 function buttonPush(array) {
-    for (var i = 0; i < topics.length; i++) {
+    for (var i = 0; i < array.length; i++) {
         document.getElementById("buttons").innerHTML += '<button type="button" class="btn btn-secondary" id="' + array[i] + '">' + array[i] + " " + '</button>';
-        gifPush();
     }
 };
 
-$("#searchButton").click(function () {
-    var value = $("#searchBox").val();
-    $("#buttons").append('<button type="button" class="btn btn-secondary" id="' + value + '">' + value + '</button>');
-    
-});
+function newButton(boxSearch) {
+    document.getElementById("buttons").innerHTML += '<button type="button" class="btn btn-secondary" id="' + boxSearch + '">' + boxSearch + " " + '</button>';
+};
 
+function searchFunction() {
+    $("#searchButton").on("click", function (event) {
+        event.preventDefault();
+        var value = $("#searchBox").val();
+        newButton(value);
+        gifPush(this);
+    });
+};
 
-buttonPush(topics);
+    buttonPush(topics);
+    gifPush();
+    searchFunction();
 
 
