18d9ba8ac5ba299938b7f880a95803e28b8ce9ac
aaron <aaronbushman@gmail.com>
2019-06-24 21:34:26 -0400

    initial arrays to js file

---
+++ giphy.js
@@ -0,0 +1,5 @@
+// create array of buttons to initialy create with loop to page screen
+var topics = ["funkadelic", "james brown", "mc5", "iggy pop", "john lennon", "neil young", "sky saxon", "brian wilson", "miles davis", "john coltrane", "sun ra", "velvet underground"];
+
+
+
